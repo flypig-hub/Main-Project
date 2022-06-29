3935c81ded31a3f76de9ab40796c7a5e15cfa36c
wea9677 <wea9677@naver.com>
2022-06-29 14:21:25 +0900

    네이버, 구글, 카카오 파일 분리

--- controller/userController.js
+++ controller/userController.js
@@ -24,10 +24,55 @@ const kakaoCallback = (req, res, next) => {
 }
 // 구글 로그인
 
+const googleCallback = (req, res, next) => {
+  passport.authenticate(
+      'google',
+      { failureRedirect: '/' },
+      (err, user, info) => {
+          if (err) return next(err)
+          console.log('콜백')
+          const { userId, nickName, userImg } = user
+          const token = jwt.sign({ userId }, process.env.MY_KEY)
+
+          result = {
+              token,
+              userId,
+              nickName,
+              userImg
+          }
+          console.log('구글 콜백 함수 결과', result)
+          res.send({ user: result })
+      }
+  )(req, res, next)
+}
+
 
 
 
 // 네이버 로그인
+
+const naverCallback = (req, res, next) => {
+  passport.authenticate(
+      'naver',
+      { failureRedirect: '/' },
+      (err, user, info) => {
+          if (err) return next(err)
+          console.log('콜백')
+          const { userId, nickName, userImg } = user
+          const token = jwt.sign({ userId }, process.env.MY_KEY)
+
+          result = {
+              token,
+              userId,
+              nickName,
+              userImg
+          }
+          console.log('네이버 콜백 함수 결과', result)
+          res.send({ user: result })
+      }
+  )(req, res, next)
+}
+
 
 
 async function checkMe(req, res) {
@@ -41,5 +86,7 @@ async function checkMe(req, res) {
   };
 
 
-module.exports.kakaoCallback = kakaoCallback;
-module.exports.checkMe = checkMe;
+module.exports = {
+  kakaoCallback, googleCallback, naverCallback,
+  checkMe
+}
--- router/userRouter.js
+++ router/userRouter.js
@@ -1,18 +1,34 @@
 const express = require("express");
 const passport = require("passport")
 const authMiddleware = require("../middlewares/auth-middleware");
-const userController = require("../controller/userController");
 const router = express.Router();
 
 const {
     kakaoCallback,
+    naverCallback,
+    googleCallback,
     checkMe
 } = require("../controller/userController")
 
 
 //카카오 로그인
-router.get('/kakao', kakaoCallback);
-router.get('/kakao/callback', passport.authenticate('kakao'));
+router.get('/kakao', passport.authenticate('kakao'));
+router.get('/kakao/callback', kakaoCallback);
+
+
+
+//구글 로그인
+
+router.get('/google', passport.authenticate('google', {scope: ['profile'],}))
+router.get('/google/callback', googleCallback)
+
+
+
+
+//네이버 로그인
+router.get('/naver', passport.authenticate('naver', { authType: 'reprompt' }))
+router.get('/naver/callback', naverCallback)
+
 
 
 
