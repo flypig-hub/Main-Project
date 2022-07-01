0c3f2e35c525b91ffc9fd1e970c3aa5ca61bbdfa
wea9677 <wea9677@naver.com>
2022-07-01 10:11:05 +0900

    env 파일 수정 및 auth-middleware 암호화

--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -12,7 +12,7 @@ module.exports = async (req, res, next) => {
         return;
     }
     try {
-        const { userId } = jwt.verify(tokenValue, 'my-secret-key');
+        const { userId } = jwt.verify(tokenValue, process.env.MY_KEY);
         const user = await UserDB.findById(userId);
         
         res.locals.user = user;
