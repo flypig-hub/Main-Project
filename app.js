da83302c4e55797e9ca054ad5319478858063e68
강유신 <96379177+Usiniverse@users.noreply.github.com>
2022-07-05 14:48:54 +0900

    서버 확인 중

--- app.js
+++ app.js
@@ -40,10 +40,10 @@ app.use("/user", UserRouter);
 app.use("/post", PostRouter, CommentRouter, LikeRouter);
 
 app.set("view engine", "pug", "ejs");
-// app.set("views", __dirname + "/views");
-// app.use("/public", express.static(__dirname + "/public"));
-// app.get("/", (_, res) => res.render("home"));
-// app.get("/*", (_, res) => res.redirect("/"));
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 
 
