4e11f8060a2a31ba0286281d03cf1c977b6bd731
wea9677 <wea9677@naver.com>
2022-07-07 15:16:09 +0900

    주석처리

--- passport/index.js
+++ passport/index.js
@@ -8,13 +8,13 @@ const { users } = require('../models/index');
 module.exports = () => {
     passport.serializeUser((users, done) => {
         // req.login(user, ...)의 user가 넘어와 값을 이용할수 있다.
-        console.log('직렬화', users[0].snsId);
+        // console.log('직렬화');
         done(null, users);
     });
     passport.deserializeUser((users, done) => {
         // req.session에 저장된 사용자 아이디를 바탕으로 DB 조회로 사용자 정보를 얻어낸 후 req.user에 저장.
         // 즉, id를 sql로 조회해서 전체 정보를 가져오는 복구 로직이다.
-        console.log('역직렬화', users);
+        // console.log('역직렬화', users);
         done(null, users);
     });
 
