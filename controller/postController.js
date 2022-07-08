98885205f51941226b11b0a9f73e9f7d0233a81f
강유신 <rkddbtls007@naver.com>
2022-07-09 00:20:14 +0900

    check

--- controller/postController.js
+++ controller/postController.js
@@ -7,7 +7,7 @@ const multiparty = require("multiparty");
 async function WritePosting (req, res) {
     // try{
         // const { userId, snsId, nickname } = res.locals;
-        const { title } = req.body;
+        const { title, content, tripLocation } = req.body;
         console.log(req.body);
         const image = req.files;
         console.log(req.files);
@@ -19,9 +19,7 @@ async function WritePosting (req, res) {
         const thumbnailURL = postImageURL[0];
 
         const postInfo = await posts.create({ 
-            title,
-            // title, content, tripLocation, 
-            // thumbnailString, postImageString,
+            title, content, tripLocation, 
             thumbnailURL: thumbnailURL.toString(),
             thumbnailKEY: thumbnailKEY.toString(),
             postImageURL: postImageURL.toString(),
