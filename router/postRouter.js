1b8ea401ef441432ce13d512ffb7e367cee2a66c
강유신 <rkddbtls007@naver.com>
2022-07-13 22:12:36 +0900

    check post

--- router/postRouter.js
+++ router/postRouter.js
@@ -6,7 +6,7 @@ const upload = require("../middlewares/S3-middleware");
 const router = express.Router();
 
 // 게시글 작성 API
-router.post('/', upload.array('images', 8), PostController.WritePosting);
+router.post('/', authMiddleware, upload.array('images', 8), PostController.WritePosting);
 
 
 // 게시글 조회 API
@@ -14,11 +14,11 @@ router.get('/', PostController.GetPostingList);
 
 
 // 게시글 상세 조회 API
-router.get('/:postId', PostController.GetPost);
+router.get('/:postId', authMiddleware, PostController.GetPost);
 
 
 // 게시글 수정 API
-router.patch('/:postId', upload.array('images', 8), PostController.ModifyPosting);
+router.patch('/:postId', authMiddleware, upload.array('images', 8), PostController.ModifyPosting);
 
 
 // ******************************************************************
@@ -26,7 +26,7 @@ router.patch('/:postId', upload.array('images', 8), PostController.ModifyPosting
 // router.delete('/:postId', PostController.DeletePost);
 
 // 이미지 업로드
-router.post('/images', upload.array('images', 8), ImageController.PostImage)
+router.post('/images', authMiddleware, upload.array('images', 8), ImageController.PostImage)
 
 // 이미지 가져오기
 // router.get('/images', ImageController.GetImages)
