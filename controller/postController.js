054880b03a8beee2a4a8b3326fbecc95e4b1073a
강유신 <96379177+Usiniverse@users.noreply.github.com>
2022-07-14 22:33:28 +0900

    Update postController.js

--- controller/postController.js
+++ controller/postController.js
@@ -93,10 +93,21 @@ async function ModifyPosting (req, res) {
         const existPost = await posts.findOne({
             where: { postId },
         });
+
+        if (userId !== existPost.userId) {
+            await res.status(400).send({ errorMessage: "접근 권한이 없습니다!"});
+        };
+
         const postImageKEY = image.map(postImageKEY => postImageKEY.key);
         const postImageURL = image.map(postImageURL => postImageURL.location);
         const thumbnailKEY = postImageKEY[0];
         const thumbnailURL = postImageURL[0];
+
+        if (!image.postImageURL) {
+            await posts.findOne({ where:{ thumbnailURL } });
+            res.send({ thumbnailURL });
+        };
+        
         // const imageURL = existPost.postImageURL.map(imageURL => imageURL.split('com/')[1]);
         // const s3 = new AWS.S3();
         // const params = {
@@ -112,18 +123,8 @@ async function ModifyPosting (req, res) {
         // })
     
         // if (nickname !== existPost.nickname) {
-        //         await res.status(400).send({ errorMessage: "접근 권한이 없습니다!"});
-        //     };
-    
-        // const ModifyPost = await existPost.update({
-        //     title, 
-        //     content, 
-        //     tripLocation,
-        //     postImageKEY: postImageKEY.toString(),
-        //     postImageURL: postImageURL.toString(),
-        //     category, type, link, houseTitle,
-        //     order: [["updatedAt", "DESC"]]
-        // });
+        //     await res.status(400).send({ errorMessage: "접근 권한이 없습니다!"});
+        // };
 
         const ModifyPost = await existPost.update({ 
             userId, userImage, nickname,
@@ -133,13 +134,13 @@ async function ModifyPosting (req, res) {
             postImageURL: postImageURL.toString(),
             postImageKEY: postImageKEY.toString(),
         });
-
         
         res.status(200).send({ ModifyPost, msg: "게시글이 수정되었습니다!"});
     // } catch (e) {
     //     res.status(400).send({ errorMessage: "게시글을 수정을 할 수 없습니다." });
     // }
 };
+
 // 게시글 삭제 (S3 이미지 삭제 기능 추가 예정)
 async function DeletePost (req, res) {
     // try {
