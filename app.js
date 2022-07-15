07fabd70e281c475b1e1d5488c818feffff6ae41
강유신 <rkddbtls007@naver.com>
2022-07-16 01:45:21 +0900

    check app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const webSocket = require("./socket");
 
 const corsOption = {
   origin: ["http://localhost:3000", "*",
-  "https://mendorong-jeju.co.kr/"],
+  "https://mendorong-jeju.co.kr"],
   credentials: true,
 };
 
