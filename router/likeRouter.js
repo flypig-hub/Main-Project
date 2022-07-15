36f52ce0c44f2415da6f4b5fbb7e7b9fc0241ffa
flypig-hub <103496007+flypig-hub@users.noreply.github.com>
2022-07-15 16:45:22 +0900

    likerouter-url

--- router/likeRouter.js
+++ router/likeRouter.js
@@ -4,7 +4,7 @@ const likeController = require("../controller/likeController");
 const router = express.Router();
 
 
-router.post('/:postId/like', authMiddleware, likeController.onlike);
+router.post('/:postId', authMiddleware, likeController.onlike);
 
 
 router.delete('/:postId/unlike', authMiddleware, likeController.unlike);
