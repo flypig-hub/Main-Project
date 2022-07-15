add727b7f5b66e17787d3e405107c543c9aaa98b
flypig-hub <103496007+flypig-hub@users.noreply.github.com>
2022-07-15 18:02:00 +0900

    islike

--- controller/postController.js
+++ controller/postController.js
@@ -11,7 +11,6 @@ const multiparty = require("multiparty");
 const AWS = require("aws-sdk");
 const { post } = require("../router/likeRouter");
 
-
 // 게시글 작성(유저)
 async function WritePosting(req, res) {
   // try {
@@ -41,7 +40,7 @@ async function WritePosting(req, res) {
 
   const thumbnailKEY = postImageKEY[0];
   const thumbnailURL = postImageURL[0];
-  let isLike= false;
+  let isLike = false;
   const postInfo = await posts.create({
     userId,
     userImage,
@@ -90,10 +89,13 @@ async function GetPostingList(req, res) {
     } else {
       islike = false;
     }
-    Object.assign(post, { likeNum: likeNum, commentNum: commentNum, islike:islike });
+    Object.assign(post, {
+      likeNum: likeNum,
+      commentNum: commentNum,
+      islike: islike,
+    });
     // return
   }
-  
 
   res.send({ allPost });
 }
@@ -102,34 +104,30 @@ async function GetPostingList(req, res) {
 async function GetPost(req, res) {
   const { nickname, userId } = res.locals;
   const { postId } = req.params;
-let post = await posts.findAll({ where: { postId: postId } });
-  
-  
-    const postComments = await Comments.findAll({
-      where: { postId: post[0].postId },
-    });
-    const postLikes = await Like.findAll({ where: { postId: post[0].postId } });
-    let islike = await Like.findOne({
-      where: { userId: post[0].userId, postId: post[0].postId },
-    });
-    const likeNum = postLikes.length;
-    const commentNum = postComments.length;
-    if (islike) {
-      islike = true;
-    } else {
-      islike = false;
-    }
+  let post = await posts.findAll({ where: { postId: postId } });
+
+  const postComments = await Comments.findAll({
+    where: { postId: post[0].postId },
+  });
+  const postLikes = await Like.findAll({ where: { postId: post[0].postId } });
+  let islike = await Like.findOne({
+    where: { userId: post[0].userId, postId: post[0].postId },
+  });
+  const likeNum = postLikes.length;
+  const commentNum = postComments.length;
+  if (islike) {
+    islike = true;
+  } else {
+    islike = false;
+  }
   Object.assign(post[0], {
     likeNum: likeNum,
     commentNum: commentNum,
     islike: islike,
   });
-    // return
-  
-    
-    // return
-  
-  
+  // return
+
+  // return
 
   // const comments = await Comment.findOne({ where: { postId },
   //     order : [[ "createdAt", "DESC" ]]
@@ -156,57 +154,75 @@ let post = await posts.findAll({ where: { postId: postId } });
 
 // 게시글 수정 (S3 기능 추가 예정)
 
-async function ModifyPosting (req, res) {
-    // try {
-        const { userId, userImage, nickname } = res.locals;
-        const { postId } = req.params;
-        const { title, content, mainAddress, subAddress, category, type, link, houseTitle, imageKEY } = req.body;
-        const image = req.files;
+async function ModifyPosting(req, res) {
+  // try {
+  const { userId, userImage, nickname } = res.locals;
+  const { postId } = req.params;
+  const {
+    title,
+    content,
+    mainAddress,
+    subAddress,
+    category,
+    type,
+    link,
+    houseTitle,
+    imageKEY,
+  } = req.body;
+  const image = req.files;
 
-        const existPost = await posts.findOne({
-            where: { postId },
-        });
+  const existPost = await posts.findOne({
+    where: { postId },
+  });
 
-        if (nickname !== existPost.nickname) {
-            return res.status(400).send({ errorMessage: "접근 권한이 없습니다!"});
-        };
+  if (nickname !== existPost.nickname) {
+    return res.status(400).send({ errorMessage: "접근 권한이 없습니다!" });
+  }
 
-        const thumbnailURL = image.map(thumbnailURL => thumbnailURL.location);
+  const thumbnailURL = image.map((thumbnailURL) => thumbnailURL.location);
 
-        if (image.length === 0) {
-            await posts.findOne({ where:{ thumbnailURL } });
-            return res.send({ thumbnailURL });
-        };
-        
-        // const imageURL = existPost.postImageURL.map(imageURL => imageURL.split('com/')[1]);
-        // const s3 = new AWS.S3();
-        // const params = {
-        //     Bucket: process.env.AWS_BUCKET_NAME,
-        //     Delete: { 
-        //         Objects: image.map((imageKey) => ({ Key: imageKey })) 
-        //     }
-        // };
-        // console.log(params);
-        // s3.deleteObjects(params, function(err, data) {
-        //     if (err) console.log(err, err.stack);
-        //     else { console.log("삭제되었습니다.") }
-        // })
+  if (image.length === 0) {
+    await posts.findOne({ where: { thumbnailURL } });
+    return res.send({ thumbnailURL });
+  }
 
-        const ModifyPost = await existPost.update({ 
-            userId, userImage, nickname,
-            title, content, mainAddress, subAddress, category, type, link, houseTitle,
-            thumbnailURL: thumbnailURL.toString(),
-            // thumbnailKEY,
-            // postImageURL: postImageURL.toString(),
-            // postImageKEY: postImageKEY.toString(),
-        });
-        
-        res.status(200).send({ ModifyPost, msg: "게시글이 수정되었습니다!"});
-    // } catch (e) {
-    //     res.status(400).send({ errorMessage: "게시글을 수정을 할 수 없습니다." });
-    // }
-};
+  // const imageURL = existPost.postImageURL.map(imageURL => imageURL.split('com/')[1]);
+  // const s3 = new AWS.S3();
+  // const params = {
+  //     Bucket: process.env.AWS_BUCKET_NAME,
+  //     Delete: {
+  //         Objects: image.map((imageKey) => ({ Key: imageKey }))
+  //     }
+  // };
+  // console.log(params);
+  // s3.deleteObjects(params, function(err, data) {
+  //     if (err) console.log(err, err.stack);
+  //     else { console.log("삭제되었습니다.") }
+  // })
 
+  const ModifyPost = await existPost.update({
+    userId,
+    userImage,
+    nickname,
+    title,
+    content,
+    mainAddress,
+    subAddress,
+    category,
+    type,
+    link,
+    houseTitle,
+    thumbnailURL: thumbnailURL.toString(),
+    // thumbnailKEY,
+    // postImageURL: postImageURL.toString(),
+    // postImageKEY: postImageKEY.toString(),
+  });
+
+  res.status(200).send({ ModifyPost, msg: "게시글이 수정되었습니다!" });
+  // } catch (e) {
+  //     res.status(400).send({ errorMessage: "게시글을 수정을 할 수 없습니다." });
+  // }
+}
 
 // 게시글 삭제 (S3 이미지 삭제 기능 추가 예정)
 async function DeletePost(req, res) {
