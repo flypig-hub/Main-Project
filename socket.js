7be8f91887173af9246aaf9794252765713190b8
flypig-hub <103496007+flypig-hub@users.noreply.github.com>
2022-07-19 15:48:29 +0900

    socket.to(roomName)삭제

--- socket.js
+++ socket.js
@@ -46,7 +46,8 @@ app.set("io", io);
       //roomName이름을 가진 방을 만들거나 들어감.
       socket.nickName = nickName
 
-      socket.to(roomName).emit("welcome", socket.nickname, roomName);
+      socket.emit("welcome", socket.nickname, roomName);
+      // socket.to(roomName).emit("welcome", socket.nickname, roomName);
       // io.sockets.emit("room_change");
     });
       
