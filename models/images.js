79afea40d6d714d31217fb677e589e9de5f17fe1
socket <ubuntu@ip-172-31-46-135.ap-northeast-2.compute.internal>
2022-07-19 15:12:38 +0900

    socket-ubuntu

--- models/images.js
+++ models/images.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      images.belongsTo(models.posts, { foreignKey: 'postId', sourceKey: 'postNumber', onDelete: 'CASCADE' });
     }
   }
   images.init({
