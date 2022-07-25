c4474c338a44c8be77b53f22a5a4cf166a482bd9
flypig-hub <103496007+flypig-hub@users.noreply.github.com>
2022-07-26 00:46:07 +0900

    Update roomRouter.js

--- router/roomRouter.js
+++ router/roomRouter.js
@@ -5,11 +5,11 @@ const router = express.Router();
 
 router.get("/chat/:roomId",  authMiddleware,  roomController.callchats);
 router.get("/", roomController.allRoomList);
-// router.get("/search/:roomId", authMiddleware, roomController.searchRoom); 검색 기능
-// router.get("/search//hashTag:roomId", authMiddlewareroomController.searchRoombyhashtag); 해쉬태그 검색
+router.get("/search/:roomId", authMiddleware, roomController.searchRoom); 
+router.get("/search/hashTag/:roomId", authMiddleware,roomController.searchRoombyhashtag); 
 router.get("/:roomId", authMiddleware, roomController.Roomdetail);
 router.post("/", authMiddleware, roomController.createRoom);
 router.post("/:roomId", authMiddleware, roomController.enterRoom);
 router.delete("/:roomId", authMiddleware, roomController.exitRoom);
-// router.delete("/:roomId", authMiddleware, roomController.kickUser);
+// router.delete("/", roomController.kickUser);
 module.exports = router;
