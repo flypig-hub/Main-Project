c628ec278a15c44b95eea54c3f43c804447ab713
wea9677 <wea9677@naver.com>
2022-07-28 04:26:14 +0900

    try catch 활성화 및 console.log 삭제

--- controller/reviewController.js
+++ controller/reviewController.js
@@ -41,7 +41,7 @@ async function writeReview(req, res) {
         });
         return;
     }
-//try {
+try {
     if(!review) {
         res.status(412).send({
           errorMessage:"후기를 입력해 주세요.",
@@ -62,9 +62,9 @@ async function writeReview(req, res) {
     })
 
     res.status(201).send({review_r, rimg, msg : "후기 작성 완료!"});
-// } catch (error) {
-//     res.status(400).send({result:false, errorMessage: "후기 작성을 할 수 없습니다."});
-// }
+} catch (error) {
+    res.status(400).send({result:false, errorMessage: "후기 작성을 할 수 없습니다."});
+}
 }
 
 //review 수정
@@ -79,7 +79,7 @@ try {
     const existReview = await reviews.findOne({
         where: { reviewId },
     });
-    console.log(existReview, ' 로그 찍어보자');
+    
     if (!userId) {
         res.status(400).send({errorMessage:"로그인 후 수정 할 수 있습니다."});
         return;
@@ -106,13 +106,13 @@ try {
 async function deleteReivew(req, res) {
     const { userId } = res.locals;
     const { reviewId } = req.params;
-//try {
+try {
     const existReview = await reviews.findOne({
         where: {
             userId,
         },
     });
-    console.log(existReview, '작성된 리뷰');
+    
     if (!userId) {
         res.status(400).send({errorMessage:"로그인 후 수정 할 수 있습니다."});
         return;
@@ -130,9 +130,9 @@ async function deleteReivew(req, res) {
         return;
     }
     res.status(200).send({msg:"후기 삭제 완료!"});
-// } catch (error) {
-//     res.status(400).send({errorMessage: "후기를 삭제 할 수 없습니다."});
-//   }
+} catch (error) {
+    res.status(400).send({errorMessage: "후기를 삭제 할 수 없습니다."});
+  }
 }
 
 module.exports = {deleteReivew, updateReview, writeReview, readReview}
