6c8dd967f537489ab13a2ce0a30739353281088c
wea9677 <wea9677@naver.com>
2022-07-28 04:24:25 +0900

    try catch 및 console.log 제거

--- controller/saveController.js
+++ controller/saveController.js
@@ -1,39 +1,80 @@
-const { hosts, saves, sequelize, Sequelize } = require("../models");
+const {
+    hosts,
+    saves,
+    sequelize,
+    Sequelize
+} = require("../models");
 
 
 //게시물 저장하기
 async function onsave(req, res) {
-    const {userId} = res.locals;
-    const {hostId} = req.params;
+   try {
+      const {
+    userId
+} = res.locals;
+const {
+    hostId
+} = req.params;
 
-    const hostsave = await saves.findAll({
-        where : {userId : userId, hostId: hostId},
+const hostsave = await saves.findAll({
+    where: {
+        userId: userId,
+        hostId: hostId
+    },
+});
+
+if (hostsave[0] !== undefined) {
+    res.status(400).send({
+        errorMessage: '저장한 게시물 입니다.'
     });
+    return
+}
+const saveHost = await saves.create({
+    userId: userId,
+    hostId: hostId
+});
 
-    if (hostsave[0] !== undefined) {
-        res.status(400).send({errorMessage : '저장한 게시물 입니다.'});
-        return
-    }
-     const saveHost = await saves.create({
-        userId:userId, hostId:hostId
-     });
-
-     res.status(200).send({result : true, message : '저장완료', saveHost});  
+res.status(200).send({
+    result: true,
+    message: '저장완료',
+    saveHost
+});
+    
+   } catch (error) {
+    res.status(400).send({errorMessage:"저장실패"})
+   }
+  
 }
 
 //게시물 저장 취소하기
 
 async function saveDenied(req, res) {
-    const {userId} = res.locals;
-    const {hostId} = req.params;
-
-    const savedenied = await saves.destroy({
-        where : {
-            userId,
+    try {
+        const {
+            userId
+        } = res.locals;
+        const {
             hostId
-        },
-    });
-    res.status(200).send({result:true, message : '저장취소', savedenied})
+        } = req.params;
+    
+        const savedenied = await saves.destroy({
+            where: {
+                userId,
+                hostId
+            },
+        });
+        res.status(200).send({
+            result: true,
+            message: '저장취소',
+            savedenied
+        })
+    } catch (error) {
+        res.status(400).send({errorMessage : "저장 취소 실패"})
+    }
+   
 }
 
-module.exports = {saveDenied, onsave}
+module.exports = {
+    saveDenied,
+    onsave
+}
