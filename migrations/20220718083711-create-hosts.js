4418dc07db3eb562a2bce4d48ad3c594be3f6184
wea9677 <wea9677@naver.com>
2022-07-28 15:46:38 +0900

    hosts taglist 추가

--- migrations/20220718083711-create-hosts.js
+++ migrations/20220718083711-create-hosts.js
@@ -53,6 +53,9 @@ module.exports = {
       preImages: {
         type: Sequelize.STRING
       },
+      tagList: {
+        type: Sequelize.STRING
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
