1cc984b3aa0ea0c5095d9d04d5bd6099a6c264fd
flypig-hub <103496007+flypig-hub@users.noreply.github.com>
2022-07-29 02:45:49 +0900

    Update socket.js

--- socket.js
+++ socket.js
@@ -49,33 +49,35 @@ module.exports = (server, app) => {
           userImg: null,
         });
       }
-      if (enterRoom.dataValues.roomUserId.includes(Number(userId)))
-      {}
-        else if (enterRoom.dataValues.hostId === Number(userId))
-        {} else 
-       {
-        let userImageURL = await images.findOne({
-          attributes: ["userImageURL"],
-          where: { userId: userId },
-        });
-        enterRoom.roomUserId.push(Number(userId));
-        enterRoom.roomUserNickname.push(enterUser.dataValues.nickname);
-        let roomUserNum = enterRoom.roomUserNickname.length + 1;
-        enterRoom.roomUserImg.push(userImageURL.userImageURL);
-        if (enterRoom.dataValues.hostId !== Number(userId)){
-        await Rooms.update(
-          {
-            roomUserId: enterRoom.dataValues.roomUserId,
-            roomUserImg: enterRoom.dataValues.roomUserImg,
-            roomUserNickname: enterRoom.dataValues.roomUserNickname,
-            roomUserNum: roomUserNum,
-          },
-          { where: { roomId: roomId } }
-        );}
-        socket
-          .to(enterRoom.title)
-          .emit("welcome", enterUser.dataValues.nickname);
-      }
+      console.log("유저가 방에 들어와있나요?",enterRoom.dataValues.roomUserId.includes(Number(userId)enterRoom.dataValues.roomUserId,Number(userId))
+       console.log("호스트가 방에 들어와있나요?",enterRoom.dataValues.hostId === Number(userId), enterRoom.dataValues.hostId,Number(userId) )           
+//       if (enterRoom.dataValues.roomUserId.includes(Number(userId)))
+//       {}
+//         else if (enterRoom.dataValues.hostId === Number(userId))
+//         {} else 
+//        {
+//         let userImageURL = await images.findOne({
+//           attributes: ["userImageURL"],
+//           where: { userId: userId },
+//         });
+//         enterRoom.roomUserId.push(Number(userId));
+//         enterRoom.roomUserNickname.push(enterUser.dataValues.nickname);
+//         let roomUserNum = enterRoom.roomUserNickname.length + 1;
+//         enterRoom.roomUserImg.push(userImageURL.userImageURL);
+        
+//         await Rooms.update(
+//           {
+//             roomUserId: enterRoom.dataValues.roomUserId,
+//             roomUserImg: enterRoom.dataValues.roomUserImg,
+//             roomUserNickname: enterRoom.dataValues.roomUserNickname,
+//             roomUserNum: roomUserNum,
+//           },
+//           { where: { roomId: roomId } }
+//         );}
+//         socket
+//           .to(enterRoom.title)
+//           .emit("welcome", enterUser.dataValues.nickname);
+      
     });
 
     socket.on("chat_message", async (messageChat, userId, roomId) => {
