be7450aa21b97a5e22160efe432200c394defdcf
wea9677 <wea9677@naver.com>
2022-07-31 23:40:03 +0900

    allPost 위치수정

--- controller/postController.js
+++ controller/postController.js
@@ -234,6 +234,28 @@ async function GetPost(req, res) {
   } 
   const newTAG = newTagStr.split(',')
 
+  const allPost = allPostInfo.map((postInfo) =>({
+    postId : postInfo.postId,
+    userId : postInfo.userId,
+    nickname : postInfo.user.nickname,
+    userImageURL : postInfo.user.userImageURL,
+    content : postInfo.content,
+    title : postInfo.title,
+    commentId : postInfo.commentId,
+    commentNum : postInfo.commentNum,
+    likeNum : postInfo.likeNum,
+    islike : postInfo.islike,
+    mainAddress : postInfo.mainAddress,
+    subAddress : postInfo.subAddress,
+    category : postInfo.category,
+    type : postInfo.type,
+    link : postInfo.link,
+    houseTitle : postInfo.houseTitle,
+    tagList : newTAG,
+    createdAt : postInfo.createdAt,
+    updatedAt : postInfo.updatedAt,
+    images : postInfo.images
+  })); 
  
   console.log(allPost,'로그');
 
@@ -269,30 +291,9 @@ async function GetPost(req, res) {
     },
     { where: { postId: allPostInfo[0].postId } }
   );
-  const allPost = allPostInfo.map((postInfo) =>({
-    postId : postInfo.postId,
-    userId : postInfo.userId,
-    nickname : postInfo.user.nickname,
-    userImageURL : postInfo.user.userImageURL,
-    content : postInfo.content,
-    title : postInfo.title,
-    commentId : postInfo.commentId,
-    commentNum : postInfo.commentNum,
-    likeNum : postInfo.likeNum,
-    islike : postInfo.islike,
-    mainAddress : postInfo.mainAddress,
-    subAddress : postInfo.subAddress,
-    category : postInfo.category,
-    type : postInfo.type,
-    link : postInfo.link,
-    houseTitle : postInfo.houseTitle,
-    tagList : newTAG,
-    createdAt : postInfo.createdAt,
-    updatedAt : postInfo.updatedAt,
-    images : postInfo.images
-  })); 
+  
 
-
+//글쓴이의 다른 게시물
   
    const outherPosts = await posts.findAll({
     where: {
@@ -314,6 +315,17 @@ async function GetPost(req, res) {
       }
     ],
   });
+  const outherPostInfo = outherPosts.map((outherpostinfo) =>({
+    postId : outherpostinfo.postId,
+    userId : outherpostinfo.userId,
+    nickname : outherpostinfo.user.nickname,
+    title : outherpostinfo.title,
+    commentNum : outherpostinfo.commentNum,
+    likeNum : outherpostinfo.likeNum,
+    islike : outherpostinfo.islike,
+    preImages : outherpostinfo.preImages,
+    images : outherpostinfo.images
+   }));
 
 
   for (i = 0; outherPosts.length > i; i++){
@@ -395,17 +407,7 @@ async function GetPost(req, res) {
     
     
   }
-  const outherPostInfo = outherPosts.map((outherpostinfo) =>({
-    postId : outherpostinfo.postId,
-    userId : outherpostinfo.userId,
-    nickname : outherpostinfo.user.nickname,
-    title : outherpostinfo.title,
-    commentNum : outherpostinfo.commentNum,
-    likeNum : outherpostinfo.likeNum,
-    islike : outherpostinfo.islike,
-    preImages : outherpostinfo.preImages,
-    images : outherpostinfo.images
-   }));
+
   
     // 이 글에 나온 숙소 찾아오기
     let findHostId = await hosts.findAll({
