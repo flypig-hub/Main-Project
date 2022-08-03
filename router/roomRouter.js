c32a077e02e479781d9c01f8b609fe4b49946cf1
flypig-hub <103496007+flypig-hub@users.noreply.github.com>
2022-08-03 09:51:23 +0900

    Update roomRouter.js

--- router/roomRouter.js
+++ router/roomRouter.js
@@ -8,7 +8,7 @@ router.get("/chat/:roomId",  authMiddleware,  roomController.callchats);
 router.get("/", roomController.allRoomList);
 router.get("/search", userIdMiddleware, roomController.searchRoom); 
 router.get("/search/hashTag", userIdMiddleware,roomController.searchRoombyhashtag); 
-router.get("/:roomId", authMiddleware, roomController.Roomdetail);
+router.get("/:roomId", userIdMiddleware, roomController.Roomdetail);
 router.post("/", authMiddleware, roomController.createRoom);
 router.post("/:roomId", authMiddleware, roomController.enterRoom);
 router.delete("/:roomId", authMiddleware, roomController.exitRoom);
